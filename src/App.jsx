edbba83486e16bb2f3a26473c8de309aa6fe66ac
AkshayRawat17 <rawataksahy603@gmail.com>
2025-06-01 11:03:02 +0530

    create-counter-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
+import { decrement, increment, incrementByAmount, reset } from './features/counter/counterSlice'
+import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [amount, setAmount] = useState(0)
+  let count = useSelector((state) => state.counter.value)
+  let dispatch = useDispatch()
+
+  function handleIncrement() {
+    dispatch(increment())
+  }
+
+  function handleDecrement() {
+    dispatch(decrement())
+  }
+
+  function handleReset() {
+    dispatch(reset())
+  }
+
+  function handleIncByAmount() {
+    dispatch(incrementByAmount(amount))
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <button onClick={handleIncrement}> + </button>
+        <p>Count : {count} </p>
+        <button onClick={handleDecrement} > - </button>
+        <br />
+        <br />
+        <button onClick={handleReset} > Reset </button>
+        <br />
+        <br />
+        <input type="number" value={amount} placeholder='Enter Amount' onChange={(e) => setAmount(e.target.value)} />
+        <br />
+        <br />
+        <button onClick={handleIncByAmount} > Increment By Amount </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
